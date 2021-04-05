285c1dd526ad234f69c9d62e89571896de462931
turner <github@turner.dev>
2021-04-04 18:00:47 -0700

    Graph cycle detection

--- pkg/graph.go
+++ pkg/graph.go
@@ -1,1 +1,61 @@
 package core
+
+// Graph structures
+type Graph struct {
+	vertices      int
+	adjacencyList map[int][]int
+	reversed      map[int][]int
+}
+
+// NewGraph constructs a new Graph instance
+func NewGraph(vertices int) *Graph {
+	return &Graph{
+		vertices:      vertices,
+		adjacencyList: make(map[int][]int),
+		reversed:      make(map[int][]int),
+	}
+}
+
+// isCycle performs a recursive depth first search to identify cycles in the graph
+func (g Graph) isCyclic(v int, visited []bool, stack []bool) bool {
+	visited[v] = true
+	stack[v] = true
+
+	// If any neighbour is visited and in the stack, graph is cyclic
+	for _, w := range g.adjacencyList[v] {
+		if !visited[w] {
+			if g.isCyclic(w, visited, stack) {
+				return true
+			}
+		} else if stack[w] {
+			return true
+		}
+	}
+	stack[v] = false
+	return false
+}
+
+// IsCyclic returns true if the Graph has at least one cycle
+func (g Graph) IsCyclic() bool {
+	visited := make([]bool, g.vertices)
+	stack := make([]bool, g.vertices)
+
+	// With each vertex as a starting point, walk the graph
+	for v := range g.adjacencyList {
+		if !visited[v] {
+			if g.isCyclic(v, visited, stack) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+// AddEdge creates an edge between two vertices
+func (g Graph) AddEdge(v int, w int) {
+	if vs, ok := g.adjacencyList[v]; ok {
+		g.adjacencyList[v] = append(vs, w)
+	} else {
+		g.adjacencyList[v] = []int{w}
+	}
+}
--- pkg/graph_test.go
+++ pkg/graph_test.go
@@ -1,1 +1,32 @@
 package core
+
+import "testing"
+
+func TestGraphDetectCycles(t *testing.T) {
+
+	t.Run("1 cycle", func(t *testing.T) {
+		g := NewGraph(5)
+		g.AddEdge(0, 1)
+		g.AddEdge(1, 2)
+		g.AddEdge(2, 3)
+		g.AddEdge(3, 1)
+		g.AddEdge(3, 4)
+
+		if !g.IsCyclic() {
+			t.Errorf("Cyclic graph not detected")
+		}
+	})
+
+	t.Run("No cycle", func(t *testing.T) {
+		g := NewGraph(5)
+		g.AddEdge(0, 1)
+		g.AddEdge(1, 2)
+		g.AddEdge(2, 3)
+		g.AddEdge(3, 4)
+
+		if g.IsCyclic() {
+			t.Errorf("Cyclic graph not detected")
+		}
+	})
+
+}
