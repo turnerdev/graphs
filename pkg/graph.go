e6dd5f01a5b545bc21ff442e1fee9c386b96db7e
turner <github@turner.dev>
2021-04-04 23:58:33 -0700

    Vertex degrees

--- pkg/graph.go
+++ pkg/graph.go
@@ -1,16 +1,57 @@
 package core
 
+// Edge structure
+type Edge struct {
+	v        *Vertex
+	w        *Vertex
+	capacity int
+	flow     int
+	weight   int
+}
+
+// Vertex structure
+type Vertex struct {
+	id  int
+	in  []*Edge
+	out []*Edge
+}
+
+func (v *Vertex) addEdge(w *Vertex) *Edge {
+	edge := &Edge{
+		v: v,
+		w: w,
+	}
+	v.out = append(v.out, edge)
+	w.in = append(w.in, edge)
+	return edge
+}
+
+// Degree (or valency) is the number of edges that are incident to the Vertex
+func (v Vertex) Degree() int {
+	return v.Indegree() + v.Outdegree()
+}
+
+// Indegree is the number of head ends adjacent to the Vertex
+func (v Vertex) Indegree() int {
+	return len(v.in)
+}
+
+// Outdegree is the number of tail ends adjacent to the Vertex
+func (v Vertex) Outdegree() int {
+	return len(v.out)
+}
+
 // Graph structures
 type Graph struct {
-	vertices      int
+	vertices      map[int]*Vertex
 	adjacencyList map[int][]int
 	reversed      map[int][]int
 }
 
 // NewGraph constructs a new Graph instance
-func NewGraph(vertices int) *Graph {
+func NewGraph() *Graph {
 	return &Graph{
-		vertices:      vertices,
+		vertices:      make(map[int]*Vertex),
 		adjacencyList: make(map[int][]int),
 		reversed:      make(map[int][]int),
 	}
@@ -31,14 +72,15 @@ func (g Graph) isCyclic(v int, visited []bool, stack []bool) bool {
 			return true
 		}
 	}
+	// Pop the stack
 	stack[v] = false
 	return false
 }
 
 // IsCyclic returns true if the Graph has at least one cycle
 func (g Graph) IsCyclic() bool {
-	visited := make([]bool, g.vertices)
-	stack := make([]bool, g.vertices)
+	visited := make([]bool, len(g.vertices))
+	stack := make([]bool, len(g.vertices))
 
 	// With each vertex as a starting point, walk the graph
 	for v := range g.adjacencyList {
@@ -51,11 +93,36 @@ func (g Graph) IsCyclic() bool {
 	return false
 }
 
-// AddEdge creates an edge between two vertices
-func (g Graph) AddEdge(v int, w int) {
-	if vs, ok := g.adjacencyList[v]; ok {
-		g.adjacencyList[v] = append(vs, w)
-	} else {
-		g.adjacencyList[v] = []int{w}
+// AddVertex creates a Vertex with the given id
+func (g Graph) AddVertex(v int) *Vertex {
+	vertex := &Vertex{
+		id:  v,
+		in:  make([]*Edge, 0),
+		out: make([]*Edge, 0),
 	}
+	g.vertices[v] = vertex
+	g.adjacencyList[v] = make([]int, 0)
+	return vertex
+}
+
+// GetVertex returns the Vertex with the given id
+func (g Graph) GetVertex(v int) *Vertex {
+	return g.vertices[v]
+}
+
+// AddEdge creates an edge between two vertices
+func (g Graph) AddEdge(v int, w int) *Edge {
+	vertexA, ok := g.vertices[v]
+	if !ok {
+		vertexA = g.AddVertex(v)
+	}
+	vertexB, ok := g.vertices[w]
+	if !ok {
+		vertexB = g.AddVertex(w)
+	}
+	edge := vertexA.addEdge(vertexB)
+
+	g.adjacencyList[v] = append(g.adjacencyList[v], w)
+
+	return edge
 }
--- pkg/graph_test.go
+++ pkg/graph_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestGraphDetectCycles(t *testing.T) {
 
 	t.Run("1 cycle", func(t *testing.T) {
-		g := NewGraph(5)
+		g := NewGraph()
 		g.AddEdge(0, 1)
 		g.AddEdge(1, 2)
 		g.AddEdge(2, 3)
@@ -18,7 +18,7 @@ func TestGraphDetectCycles(t *testing.T) {
 	})
 
 	t.Run("No cycle", func(t *testing.T) {
-		g := NewGraph(5)
+		g := NewGraph()
 		g.AddEdge(0, 1)
 		g.AddEdge(1, 2)
 		g.AddEdge(2, 3)
@@ -26,6 +26,42 @@ func TestGraphDetectCycles(t *testing.T) {
 
 		if g.IsCyclic() {
 			t.Errorf("Cyclic graph not detected")
+		}
+	})
+
+}
+
+func TestDegree(t *testing.T) {
+	g := NewGraph()
+	g.AddEdge(0, 1)
+	g.AddEdge(0, 2)
+	g.AddEdge(1, 2)
+	g.AddEdge(2, 3)
+	g.AddEdge(2, 3)
+	g.AddEdge(2, 5)
+	g.AddEdge(3, 5)
+
+	t.Run("Test vertex degree", func(t *testing.T) {
+		want := 5
+		got := g.GetVertex(2).Degree()
+		if want != got {
+			t.Errorf("degree is %d ; want %d", got, want)
+		}
+	})
+
+	t.Run("Test vertex indegree", func(t *testing.T) {
+		want := 2
+		got := g.GetVertex(2).Indegree()
+		if want != got {
+			t.Errorf("degree is %d ; want %d", got, want)
+		}
+	})
+
+	t.Run("Test vertex outdegree", func(t *testing.T) {
+		want := 3
+		got := g.GetVertex(2).Outdegree()
+		if want != got {
+			t.Errorf("degree is %d ; want %d", got, want)
 		}
 	})
 
